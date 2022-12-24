e52a30c5962de41ab4e0e34cdb4f8cc632701de9
Dave Behnke <916775+dbehnke@users.noreply.github.com>
2022-12-23 23:15:10 -0500

    update debian build

--- docker/debian-build/build-svxlink.sh
+++ docker/debian-build/build-svxlink.sh
@@ -29,6 +29,7 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_SYSCONFDIR=/etc \
       -DCMAKE_INSTALL_LOCALSTATEDIR=/var \
+      -DUSE_QT=0 \
       -DCMAKE_BUILD_TYPE=Release ../svxlink/src
 make -j$num_cores
 sudo make install
